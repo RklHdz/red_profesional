474601982117445967b200b1e34f24eba581ce8d
Enrique <enrique.qzada@gmail.com>
2018-11-16 14:16:33 -0600

    configuraciones generales
    se realizo las configuraciones generales del proyecto

--- application/config/config.php
+++ application/config/config.php
@@ -24,6 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://localhost/red_profesional';
+$config['site_url'] = 'http://localhost/red_profesional';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +36,7 @@ $config['base_url'] = 'http://localhost/red_profesional';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
