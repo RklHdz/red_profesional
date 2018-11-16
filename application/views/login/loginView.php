b0ba2fc1b12a24afef0beb4130e5fbd5246f9f93
Enrique <enrique.qzada@gmail.com>
2018-11-16 14:19:12 -0600

    login
    se hizo el controlador y la vista del login
    asi como la estructuracción de las librerias de css y js referente al login

---
+++ application/views/login/loginView.php
@@ -0,0 +1,44 @@
+
+
+<body>
+	<!--[if lt IE 8]>
+		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+	<![endif]-->
+	<div class="error-pagewrap">
+		<div class="error-page-int">
+			<div class="text-center m-b-md custom-login">
+				<h3></h3>
+				<p>This is the best app ever!</p>
+			</div>
+			<div class="content-error">
+				<div class="hpanel">
+					<div class="panel-body">
+						<form action="#" id="loginForm">
+							<div class="form-group">
+								<label class="control-label" for="username">Usuario</label>
+								<input type="text" placeholder="example@gmail.com" title="Please enter you username" required="" value="" name="username" id="username" class="form-control">
+								<!--<span class="help-block small">usuario</span>-->
+							</div>
+							<div class="form-group">
+								<label class="control-label" for="password">Contraseña</label>
+								<input type="password" title="Please enter your password" placeholder="******" required="" value="" name="password" id="password" class="form-control">
+								<!--<span class="help-block small">Yur strong password</span>-->
+							</div>
+							<!--<div class="checkbox login-checkbox">
+								<label>
+								<input type="checkbox" class="i-checks"> Remember me </label>
+								<p class="help-block small">(if this is a private computer)</p>
+							</div>-->
+							<button class="btn btn-success btn-block loginbtn">Iniciar sesión</button>
+							<a class="btn btn-default btn-block" href="#">Registro</a>
+						</form>
+					</div>
+				</div>
+			</div>
+			<div class="text-center login-footer">
+				<p>Copyright © 2018. All rights reserved. Template by <a href="https://colorlib.com/wp/templates/">Colorlib</a></p>
+			</div>
+		</div>   
+	</div>
+	
+	
