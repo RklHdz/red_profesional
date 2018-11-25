d55191c0c36520dca671210abe221f320525b012
Enrique Quezada <enrique.qzada@gmail.com>
2018-11-24 23:30:04 -0600

    creacion del modelo Login_model.php
    se creo el archivo y la clase Login_model, el cual se encargara de realizar las diversas consultas a la base de datos

---
+++ application/models/Login_model.php
@@ -0,0 +1,16 @@
+<?php 
+
+	defined('BASEPATH') OR exit('No direct script access allowed');
+
+	/**
+	 * esta clase se encargara de hacer las consultas a la base de datos
+	   referente a la tabla login
+	 */
+	class Login_model extends CI_Model
+	{
+		
+		
+	}
+
+
+
