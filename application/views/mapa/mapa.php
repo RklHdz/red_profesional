3488296eb13646f7b2e91b940e24592646ae38c6
EsaSanchez <41446929+EsaSanchez@users.noreply.github.com>
2019-01-07 14:11:03 -0600

    Cambio de codigo de mapa
    Incluí todo el diseño y el script en la misma pagina de php porque si lo hacia externo no me corria el mapa

--- application/views/mapa/mapa.php
+++ application/views/mapa/mapa.php
@@ -1,25 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
+     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.4.0/dist/leaflet.css"
+   integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
+   crossorigin=""/> 
 	<script src="https://unpkg.com/leaflet@1.0.2/dist/leaflet.js"></script>
 	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.2/dist/leaflet.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/map.css') ?>">
-	<title></title>
+	<style>
+  #map { 
+    width: 100%;
+    height: 580px;
+    box-shadow: 5px 5px 5px #888;
+ }
+</style>
 </head>
-<body>
-<div id="map"></div>
+	<body>
+		<div id = 'map'>
+		</div>
+	</body>
+	<script>
+  var map = L.map('map').
+     setView([13.702304199999999, -89.20085569999999],
+     15);
 
-<script>
-	var map = L.map('map')// Se declara la variable para la utilizacion del mapa
-		setView([41.66, -4.72], 15);//Se establece la posicion inicial del mapa
-       //Se añade las capas del mapa
-		L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
-		{ attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-		maxZoom: 18}).addTo(map);
+     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+maxZoom: 18
+}).addTo(map);
 
-		L.control.scale().addTo(map);//Se añade un control de escala
+     L.control.scale().addTo(map);
 
-		L.marker([41.66, -4.71], {draggable: true}).addTo(map);
+     L.marker([13.702304199999999, -89.20085569999999],{draggable: true}).addTo(map);
 </script>
-</body>
 </html>
+
+
