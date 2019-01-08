d5163d6919acaa6a4e6066e5dbcc4b21430ee2f2
Enrique <enrique.qzada@gmail.com>
2019-01-08 16:00:44 -0600

    pruebas

--- application/views/participantes/cv_view.php
+++ application/views/participantes/cv_view.php
@@ -1,99 +1,92 @@
 <div id="informacion_general">
 	<div id="opciones_cv">
-		<h1>Opciones</h1>
+		<center><h1><strong>Hoja de vida</strong></h1></center>
 	</div>
-	<div class="progress">
-  <div class="progress-bar" role="progressbar" style="width: 0%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">5%</div>
 </div>
-</div>
-<!-- sección que tendra las diversas partes del cv para guardar -->
-<section>
-	<span id="opciones" style="float: left;width: 35%;margin: auto;background: #ffff">
-		<div class="row mt">
-			<div class="col-md-12">	
-				<div class="panel ">
-					<div id="list-example" class="list-group">
-					  <a class="list-group-item list-group-item-action" href="#list-item-1">Información Personal</a>
-					  <a class="list-group-item list-group-item-action" href="#list-item-2">Educación</a>
-					  <a class="list-group-item list-group-item-action" href="#list-item-3">Formación Tecnica</a>
-					  <a class="list-group-item list-group-item-action" href="#list-item-4">Experiencia Laboral</a>
-					  <a class="list-group-item list-group-item-action" href="#list-item-5">Práctica Laboral</a>
-					  <a class="list-group-item list-group-item-action" href="#list-item-6">Conocimientos y Habilidades</a>
-					  <a class="list-group-item list-group-item-action" href="#list-item-7">Referencias</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</span>
-	<!--aqui iran las inputs a llenar-->
-
-	<div class="panel" style="float: right;width: 63%;margin: auto;background: #ffff">
-		<div class="row mt">
-			<div class="col-md-12">	
-				<div class="panel ">
-					<div class="panel-heading"><center><h2><strong></strong></h2></center></div>
-					<div class="panel-body">
-						<div data-spy="scroll" data-target="#list-example" data-offset="0" class="scrollspy-example">
+<div class="container" style="position: relative;">
+	<!-- sección que tendra las diversas partes del cv para guardar -->
+	<section>
+		<div class="panel" style="float: left;width: 48%;margin: auto;background: #ffff">
+			<div class="row mt">
+				<div class="col-md-12">	
+					<div class="panel ">
+						<div class="panel-heading"><center><h2><strong>Información Personal</strong></h2></center></div>
+						<div class="panel-body">
 							<div id="list-item-1">
-								<center><h4>Información Personal</h4></center>
-								<label><h4>Telefonos</h4></label>
+								<label><h4>Teléfonos</h4></label>
 								<div class="form-group row">
-									<label class="col-md-1 col-form-label" for="">Móvil:</label>
+									<label class="col-md-2 col-form-label" for="">Móvil:</label>
 									<div class="col-md-8">
 										<input class="form-control" id="movil" name="movil" type="text" readonly="">
 									</div>
 								</div>
 								<div class="form-group row">
-									<label class="col-md-1 col-form-label" for="">Casa:</label>
+									<label class="col-md-2 col-form-label" for="">Casa:</label>
 									<div class="col-md-8">
 										<input class="form-control" id="casa" name="casa" type="text" readonly="">
 									</div>
 								</div>
-								<br>
+								<br><hr>
 								<div class="form-group row">
-									<label class="col-md-1 col-form-label" for="">Email:</label>
+									<label class="col-md-2 col-form-label" for="">Email:</label>
 									<div class="col-md-8">
 										<input class="form-control" id="email" name="email" type="text" readonly="">
 									</div>
 								</div>
 								<br>
 								<div class="form-group row">
-									<label class="col-md-1 col-form-label" for="">DUI:</label>
+									<label class="col-md-2 col-form-label" for="">DUI:</label>
 									<div class="col-md-8">
 										<input class="form-control" id="dui" name="dui" type="text" readonly="">
 									</div>
 								</div>
 								<br>
 								<div class="form-group row">
-									<label class="col-md-1 col-form-label" for="">NIT:</label>
+									<label class="col-md-2 col-form-label" for="">NIT:</label>
 									<div class="col-md-8">
 										<input class="form-control" id="nit" name="nit" type="text" readonly="">
 									</div>
 								</div>
 								<br>
 								<div class="form-group row">
-									<label class="col-md-1 col-form-label" for="">Dirección:</label>
+									<label class="col-md-2 col-form-label" for="">Dirección:</label>
 									<div class="col-md-8">
 										<textarea class="form-control" name="direccion" id="direccion" cols="10" rows="2" readonly="" ></textarea>
 									</div>
 								</div>
-							</div>
-							
-							<hr>
-							<!-- Educación del usuario -->
-							<div id="informacion_p">
-								<center><h4>Educación</h4></center>
+							</div>							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>	
+		<!--aqui iran las inputs a llenar-->
+
+		<div class="panel" style="float: right;width: 48%;margin: auto;background: #ffff">
+			<div class="row mt">
+				<div class="col-md-12">	
+					<div class="panel ">
+						<div class="panel-heading"><center><h2><strong>Educación</strong></h2></center></div>
+						<div class="panel-body">
+							<div id="list-item-1">
 								<div class="form-group row">
+									<label class="col-md-3 col-form-label" for="">Carrera:</label>
 									<div class="col-md-8">
-										<textarea class="form-control" name="direccion" id="direccion" cols="10" rows="2" readonly="" ></textarea>
+										<input class="form-control" id="email" name="email" type="text" readonly="">
+									</div>
+								</div>
+								<br>
+								<div class="form-group row">
+									<label class="col-md-3 col-form-label" for="">Instituto/Universidad:</label>
+									<div class="col-md-8">
+										<input class="form-control" id="dui" name="dui" type="text" readonly="">
 									</div>
 								</div>
 							</div>
 						</div>
-						
 					</div>
 				</div>
 			</div>
-		</div>
-	</div>	
-</section>
+		</div>	
+	</section>
+</div>
