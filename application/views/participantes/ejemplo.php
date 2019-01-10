001b561ca3b0e3eb8137ca47c833d4f99995f9f9
Enrique <enrique.qzada@gmail.com>
2019-01-10 16:56:02 -0600

    curriculum paso 2
    se agrego el paso 2

--- application/views/participantes/ejemplo.php
+++ application/views/participantes/ejemplo.php
@@ -1,82 +1,193 @@
-<style>
-	ul.nav-pills {
-	    top: 80px;
-	    position: fixed;
-	}
-	div.col-sm-9 div {
-	    height: 250px;
-	    font-size: 28px;
-	}
-	 
-	  
-	@media screen and (max-width: 810px) {
-	    #section1, #section2, #section3, #section41, #section42  {
-	      margin-left: 150px;
-	    }
-	}
-</style>
-<div class="cantainer" data-spy="scroll" data-target="#myScrollspy" data-offset="20" style="position: relative;">
+<div class="root">
+	<form action="" class="form-cv">
+		<!-- aqui ira la barra de progreso -->
+		<div class="form-cv__header">
+			<ul class="progressbar">
+				<li class="progressbar__option">Paso 1</li>
+				<li class="progressbar__option">Paso 2</li>
+				<li class="progressbar__option">Paso 3</li>
+				<li class="progressbar__option">Paso 4</li>
+				<li class="progressbar__option">Paso 5</li>
+				<li class="progressbar__option">Paso 6</li>
+				<li class="progressbar__option">Paso 7</li>
+			</ul>
+			<h2 class="form-cv__titulo">HOJA DE VIDA</h2>
+		</div>
 
-	<section>
-		<div class="panel" style="float: left;width: 15%;margin: auto; ">
-			<div class="row mt">
-				<div class="col-md-12">	
-					<div class="panel ">
-						<div class="panel-heading"><center><h2><strong></strong></h2></center></div>
-						<div class="panel-body">
-							<nav class="col-sm-3" id="myScrollspy">
-							    <ul class="nav nav-pills nav-stacked">
-							        <li class="active"><a href="#section1">Educación</a></li>
-							        <li><a href="#section2">Section 2</a></li>
-							        <li><a href="#section3">Section 3</a></li>
-							        <li class="dropdown">
-							          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Section 4 <span class="caret"></span></a>
-							          <ul class="dropdown-menu">
-							            <li><a href="#section41">Section 4-1</a></li>
-							            <li><a href="#section42">Section 4-2</a></li>                     
-							          </ul>
-							        </li>
-							    </ul>
-							</nav>						
+		<!--aqui ira el formulario-->
+		<div class="form-cv__body">
+			<!-- paso 1 información general-->
+			<div class="step active" id="step-1">	
+				<div class="row mt">
+					<div class="col-md-12">	
+						<div class="panel panel-primary">
+							<div class="panel-heading"><h4>Información Personal (paso 1)</h4></div>
+							<div class="panel-body">
+								<div class="row">
+									<div class="form-group col-md-6">
+										<label for="nombre_usuario"><strong>Nombres</strong></label>
+										<input type="text" class="form-control" id="nombre_usuario"  name="nombre_usuario" placeholder="Digite sus nombres" value="<?php echo set_value('nombre_usuario'); ?>" >
+										
+										
+									</div>
+									<div class="form-group col-md-6">
+										<label for="apellido_usuario"><strong>Apellidos</strong></label>
+										<input type="text" class="form-control" id="apellido_usuario" name="apellido_usuario"  placeholder="Digite sus apellidos" value="<?php echo set_value('apellido_usuario'); ?>">
+									</div>
+								</div>
+							<!-- información de contacto -->	
+								<div class="row">
+									<h5 style="margin-left: 10px"><strong>Telefonos</strong></h5>
+									<div class="form-group col-md-3">
+										<label for="">Casa</label>
+										<input class="form-control" type="text" name="tel_casa" id="tel_casa">
+									</div>
+									<div class="form-group col-md-3">
+										<label for="">Celular</label>
+										<input class="form-control" type="text" name="tel_celular" id="tel_celular">
+									</div>
+								</div>
+								
+								<div class="row">
+									<div class="form-group col-md-3">
+										<label for="correo_usuario"><strong>Correo Electronico</strong></label>
+										<input type="text" class="form-control" id="correo_usuario" name="correo_usuario"  placeholder="ejemplo@ejemplo.com" value="<?php echo set_value('correo_usuario'); ?>">
+									</div>
+								</div>
+							<!-- Documentos de identificación -->
+								<div class="row" id="opciones_documento">
+									<div style="margin-left: 10px">
+										<h5><strong>tipo de documento de identificación</strong></h5>
+										<div class="form-check" style="margin-left: 10px">
+										  <input class="form-check-input" type="radio" name="documento_id" id="radioDUI" value="DUI">
+										  <label class="form-check-label" for="radioDUI">
+										    DUI
+										  </label>
+										</div>
+										<div class="form-check" style="margin-left: 10px">
+										  <input class="form-check-input" type="radio" name="documento_id" id="radioMinoridad" value="minoridad">
+										  <label class="form-check-label" for="radioMinoridad">
+										    Carnet Minoridad
+										  </label>
+										</div>
+									</div>
+									
+									<div class="form-group col-md-3">
+										<label for="">NIT</label>
+										<input type="text" class="form-control" id="nit" name="nit">
+									</div>
+								
+								<!--aqui va el DUI o el carnet de minoridad-->
+									<div id="docu_identi" class="form-group col-md-3">
+										
+									</div>
+									
+								</div>
+
+								<!-- licencia de conducir -->
+								<div class="row">
+									<div style="margin-left: 10px">
+										<h5>Licencia de Conducir</h5>
+										<div class="form-check" style="margin-left: 10px">
+										  <input class="form-check-input" type="radio" name="licencia" id="licencia_si" value="si">
+										  <label class="form-check-label" for="licencia_si">
+										    SI
+										  </label>
+										</div>
+										<div class="form-check" style="margin-left: 10px">
+										  <input class="form-check-input" type="radio" name="licencia" id="licencia_no" value="no">
+										  <label class="form-check-label" for="licencia_no">
+										    NO
+										  </label>
+										</div>
+									</div>
+									<!--Licencia-->
+									<div id="licencia" class="form-group col-md-3">
+										
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>	
-		<!--aqui iran las inputs a llenar-->
+			<!-- paso 2 educación-->
+			<div class="step" id="step-2">
+				<div class="row mt">
+					<div class="col-md-12">	
+						<div class="panel panel-primary">
+							<div class="panel-heading">
+								<h4>Educación (paso 2)
+									<i data-toggle="tooltip" data-placement="top" title="Agregar la educación desde la mas reciente a la mas antigua">
+										<img src="<?php echo base_url('assets/img/ayuda.png') ?>" alt="" width="20px">
+									</i>
+								</h4>
+							</div>
+							<div class="panel-body"> 
+								<div class="row">
+									<div style="margin-left: 10px">
+										<h5>¿Posee estudios?</h5>
+										<div class="form-check" style="margin-left: 10px">
+										  <input class="form-check-input" type="radio" name="estudio_pre" id="estudio_si" value="si">
+										  <label class="form-check-label" for="estudio_si">
+										    SI
+										  </label>
+										</div>
+										<div class="form-check" style="margin-left: 10px">
+										  <input class="form-check-input" type="radio" name="estudio_pre" id="estudio_no" value="no">
+										  <label class="form-check-label" for="estudio_no">
+										    NO
+										  </label>
+										</div>
+										<div id="educacion">
+											<div class="row">
+												<label class="col-md-5">Ordenar educación en orden cronológico: Iniciar con el más reciente y terminar lista con el más antiguo. Incluir bachillerato, carrera técnica o universitaria que tengas.</label>
+											</div>
+											<div class="row">
+												<div class="form-group">
+													<div class="col-md-3">
+														<!--<button type="button" class="btn btn-primary" data-toggle="modal" data-target=".modal-registro-edu">Agregar educación</button>-->
+														<button type="button" class="btn btn-info" id="btn-add-educ">Agregar educación</button>
+														<button type="button" class="btn btn-danger" id="btn-delete-educ">Eliminar</button>
+													</div>
+												</div>	
+											</div>
+											<!-- aqui iran apareciendo las diversas registro de educación-->
+											<div id="registro_educ">
+												<br>
+											</div>
+										</div>
 
-		<div class="panel" style="float: right;width: 80%;margin: auto;background: #ffff">
-			<div class="row mt">
-				<div class="col-md-12">	
-					<div class="panel ">
-						<div class="panel-heading"><center><h2><strong>Educación</strong></h2></center></div>
-						<div class="panel-body">
-							<div class="col-sm-9">
-						      <div id="section1">    
-						        <h1>Section 1</h1>
-						        <p>Try to scroll this section and look at the navigation list while scrolling!</p>
-						      </div>
-						      <div id="section2"> 
-						        <h1>Section 2</h1>
-						        <p>Try to scroll this section and look at the navigation list while scrolling!</p>
-						      </div>        
-						      <div id="section3">         
-						        <h1>Section 3</h1>
-						        <p>Try to scroll this section and look at the navigation list while scrolling!</p>
-						      </div>
-						      <div id="section41">         
-						        <h1>Section 4-1</h1>
-						        <p>Try to scroll this section and look at the navigation list while scrolling!</p>
-						      </div>      
-						      <div id="section42">         
-						        <h1>Section 4-2</h1>
-						        <p>Try to scroll this section and look at the navigation list while scrolling!</p>
-						      </div>
-						    </div>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>	
-	</section>	
+			<!-- paso 3 formacion tecnica-->
+			<div class="step" id="step-3">
+				
+			</div>
+			<!-- paso 4-->
+			<div class="step" id="step-4">
+				
+			</div>
+			<!-- paso 5-->
+			<div class="step" id="step-5">
+				
+			</div>
+			<!-- paso 6-->
+			<div class="step" id="step-6">
+				
+			</div>
+			<!-- paso 7-->
+			<div class="step" id="step-7">
+				
+			</div>
+		</div>
+		<!-- inputs escondidos -->
+		<input type="text" id="num_educacion" name="num_educacion">
+	</form>
 </div>
+
--- application/views/participantes/script/script_cv.php
+++ application/views/participantes/script/script_cv.php
@@ -1,54 +1,138 @@
 <script>
-	$('#radioDUI').click(function(){
+	$(document).ready(function(){
+		var con=0;
+		//document.getElementsByName("num_educacion")[0].value = con;
+		var num = $('#num_educacion').val(con);
+
+		if(num === '0')
+		{
+			console.log('cero');
+		}
+		else{
+			console.log('dato');
+		}
+
+		$('#radioDUI').click(function(){
 		
-		$('#dui_label').remove();
-		$('#dui_input').remove();
+			$('#dui_label').remove();
+			$('#dui_input').remove();
 
-		$('#minor_label').remove();
-		$('#minor_input').remove();
+			$('#minor_label').remove();
+			$('#minor_input').remove();
 
-		var  html='';
-		html ='<label id="dui_label" for="dui">DUI</label>'+
-			  '<input id="dui_input" type="text" class="form-control" id="dui" name="dui" placeholder="00000000-0">';
-		$('#docu_identi').append(html);
+			var  html='';
+			html = '<label id="dui_label" for="dui">DUI</label>'+
+			  	   '<input id="dui_input" type="text" class="form-control" id="dui" name="dui" placeholder="00000000-0">';
+			$('#docu_identi').append(html);
+		});
+
+		$('#radioMinoridad').click(function(){
+
+			$('#minor_label').remove();
+			$('#minor_input').remove();
+
+			$('#dui_label').remove();
+			$('#dui_input').remove();
+
+			var html='';
+			html ='<label id="minor_label" for="minoridad">Carnet Minoridad</label>'+
+				  '<input id="minor_input" type="text" class="form-control" id="minoridad" name="minoridad" placeholder="0000-000000-000-0">';
+			$('#docu_identi').append(html);
+		});
+
+
+		//para la licencia 
+		$('#licencia_si').click(function(){
+
+			$('#licencia_label').remove();
+			$('#licencia_select').remove();
+			var html='<label id="licencia_label" for="">Licencia de conducir</label>'+
+					 '<select id="licencia_select" class="form-control" name="tipo_licencia" id="tipo_licencia">'+
+						'<option value="motociclista">Motociclista</option>'+
+						'<option value="particular">Particular</option>'+
+						'<option value="liviana">Liviana</option>'+
+						'<option value="pesada">Pesada</option>'+
+						'<option value="pesada_t">Pesada T</option>'+
+					 '</select>';
+
+			$('#licencia').append(html);
+		});
+
+		$('#licencia_no').click(function(){
+
+			$('#licencia_label').remove();
+			$('#licencia_select').remove();
+		});
+
+		//para educación
+		$('#estudio_si').click(function(){
+			$('#msj_edu').remove();//borramos el msj que se genera si se selecciona no
+			var html='';
+
+			//vamos a crear el formulario
+
+
+		});
+		$('#estudio_no').click(function(){
+			var html='<h3 id="msj_edu">Sin estudios</h3>';
+			$('#msj_edu').remove();//borramos el msj que se genera si se selecciona no
+			$('#educacion').append(html);
+		});
+
+		//función anonima para eliminar un registro
+		$('#btn-delete-educ').click(function(){
+			//vamos a eliminar el ultimo elemento creado
+			$('#'+con+'').remove();
+			$('#raya-'+con+'').remove();
+			con--;
+			document.getElementsByName("num_educacion")[0].value = con;
+		})
+
+		//función anonima para agregar campos 
+		$('#btn-add-educ').click(function(){
+			//creamos un contador! el cual nos servira para saber cuantos registros se han creado
+			
+			con++;console.log(con);
+			
+			var html ='';
+
+			html = '<div class="row" id="'+con+'"> <div class="form-row">'+
+	    				'<div class="form-group col-md-6">'+
+	      					'<label for="titulo_educ-'+con+'">Título</label>'+
+	      					'<input type="text" class="form-control" id="titulo_educ-'+con+'" name="titulo_educ-'+con+'" placeholder="Título">'+
+	    				'</div>'+
+	    				'<div class="form-group col-md-6">'+
+	      					'<label for="institucion_educ-'+con+'">Institución</label>'+
+	      					'<input type="text" class="form-control" id="institucion_educ-'+con+'" name="institucion_educ-'+con+'" placeholder="Institución">'+
+	    				'</div>'+
+	  				'</div>'+
+	  				'<div class="form-row">'+
+	    				'<div class="form-group col-md-4">'+
+	      					'<label for="anio_educ-'+con+'">Año</label>'+
+	      					'<input type="text" class="form-control" id="anio_educ-'+con+'" name="anio_educ-'+con+'" placeholder="Año de estudio">'+
+	    				'</div>'+
+	    				'<div class="form-group col-md-4">'+
+	      					'<label for="nivel_educ-'+con+'">Nivel academico</label>'+
+	      					'<input type="text" class="form-control" id="nivel_educ-'+con+'" name="nivel_educ-'+con+'" placeholder="Universidad, Bachillerato, educación basica">'+
+	    				'</div>'+
+	    				'<div class="form-group col-md-4">'+
+	      					'<label for="tiempo_estudio_educ-">Tiempo de estudio</label>'+
+	      					'<input type="text" class="form-control" id="tiempo_estudio_educ-'+con+'" name="tiempo_estudio_educ-'+con+'" placeholder="Tiempo de estudio">'+
+	    				'</div>'+
+	  				'</div>'+
+	  				'<!--<div class="form-row">'+
+	    				'<div class="form-group col-md-4">'+
+	      					'<button type="button" class="btn btn-danger" id="btn-del-educ">Eliminar</button>'+
+	    				'</div>'+
+	  				'</div>--></div><hr id="raya-'+con+'">';
+	  		$('#registro_educ').append(html);
+	  		document.getElementsByName("num_educacion")[0].value = con;
+		})
+
+
+		//////////////
 	});
 
-	$('#radioMinoridad').click(function(){
-
-		$('#minor_label').remove();
-		$('#minor_input').remove();
-
-		$('#dui_label').remove();
-		$('#dui_input').remove();
-
-		var html='';
-		html ='<label id="minor_label" for="minoridad">Carnet Minoridad</label>'+
-			  '<input id="minor_input" type="text" class="form-control" id="minoridad" name="minoridad" placeholder="0000-000000-000-0">';
-		$('#docu_identi').append(html);
-	});
-
-
-	//para la licencia 
-	$('#licencia_si').click(function(){
-
-		$('#licencia_label').remove();
-		$('#licencia_select').remove();
-		var html='<label id="licencia_label" for="">Licencia de conducir</label>'+
-				 '<select id="licencia_select" class="form-control" name="tipo_licencia" id="tipo_licencia">'+
-					'<option value="motociclista">Motociclista</option>'+
-					'<option value="particular">Particular</option>'+
-					'<option value="liviana">Liviana</option>'+
-					'<option value="pesada">Pesada</option>'+
-					'<option value="pesada_t">Pesada T</option>'+
-				 '</select>';
-
-		$('#licencia').append(html);
-	});
-
-	$('#licencia_no').click(function(){
-
-		$('#licencia_label').remove();
-		$('#licencia_select').remove();
-	});
+	
 
 </script>
